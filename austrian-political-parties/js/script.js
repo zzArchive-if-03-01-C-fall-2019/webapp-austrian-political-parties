0485d6f8490340c1cb48355d081c9486caf3d518
RomeoBhuiyan <bhuiyanromeo28@gmail.com>
2020-06-04 13:09:39 +0200

    Left right assets and mechanic code
    There are now assets (left and right) for both charakters
    The mechanic is now done
    more could be added in the future

--- austrian-political-parties/js/script.js
+++ austrian-political-parties/js/script.js
@@ -130,7 +130,7 @@ for(var i = 0; i<= maxFrame; i++){
             ctx.drawImage(player1Sprites[1],player1.x,player1.y);
           }
           else {
-            ctx.drawImage(player1Sprites[2],player1.x,player1.y);
+            ctx.drawImage(player1Sprites[1],player1.x,player1.y);
           }
         }
         else if(keys[81]){
@@ -167,6 +167,68 @@ for(var i = 0; i<= maxFrame; i++){
   }
 }
 //------------------------------------------------------
+var player2Sprite = new Array(maxFrame);
+var anime2;
+
+for(var i = 0; i<= maxFrame; i++){
+  player2Sprite[i] = new Image();
+  player2Sprite[i].src = "../../assets/SebastianKurz(" + i + ").png";
+
+  if(i == maxFrame){
+    anime2 = function(){
+      if(player2.dead == false){
+        if(keys[74] && !player2.jumping){
+          ctx.drawImage(player2Sprite[1], player2.x, player2.y);
+        }
+        else if(keys[76] && !player1.jumping){
+          ctx.drawImage(player2Sprite[2], player2.x, player2.y);
+        }
+        else if(player2.jumping == true){
+          if (player2.lastDir == "l") {
+            ctx.drawImage(player2Sprites[1],player2.x,player2.y);
+          }
+          else {
+            ctx.drawImage(player2Sprites[1],player2.x,player2.y);
+          }
+        }
+        else if(keys[79]){
+          if(player1.lastDir == "l"){
+            ctx.drawImage(player2Sprites[1],(player2.x - player2.width / 2),player2.y);
+            if ((player2.x - player2.range) <= (player1.x + player1.width) &&
+              (player2.x - player2.range) >= player1.x  - (player1.width / 2) &&
+              player2.y >= player1.y &&
+              player2.y <= player1.y + player2.height) {
+                hurt(player1, player2, healthP1);
+                    displayDamage(player1.health, "p1-damage");
+                    player1.velX -= player1.health;
+                    player1.lastDir = "l";
+            }
+            else{
+              ctx.drawImage(player2Sprites[2],player2.x,player2.y);
+              if((player2.x + player2.width) + player2.range >= player1.x &&
+                (player2.x + player2.width) + player2.range <= (player1.x + (player1.width * 1.5)) &&
+                player2.y >= player1.y &&
+                player2.y <= player1.y + player1.height) {
+                  hurt(player1, player2, healthP1);
+                  displayDamage(player1.health, "p1-damage");
+                  player1.velX += player1.health;
+                  player1.lastDir = "r";
+              }
+            }
+          }
+        }
+        else{
+          ctx.drawImage(player2Sprites[0],player2.x,player2.y);
+        }
+      }
+    }
+  }
+}
+
+
+//-------------------------------------------------------------------------------
+
+
 // load player 1 sprites
 for (var i = 0; i <= maxFrames; ++i) {
     player1Sprites[i] = new Image();
@@ -244,6 +306,7 @@ for (var i = 0; i <= maxFrames; ++i) {
         };
     }
 }
+
 
 // load player 2 sprites
 for (var j = 0; j <= maxFrames; ++j) {
@@ -436,7 +499,7 @@ function update() {
 
   // render and animate characters
   anime1();
-  anim2();
+  anime2();
 
   // death animations
   if (deathTime != 0 && deathTime < 14) {
