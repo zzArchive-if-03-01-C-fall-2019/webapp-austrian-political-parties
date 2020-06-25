892116218648b00cbf2847055c4b1c8f6ed8ea57
RomeoBhuiyan <bhuiyanromeo28@gmail.com>
2020-06-25 10:43:59 +0200

    all known bugs fixed
    -asset bugs fixed
    -jump bug fixed
    -hit bug fixed
    --------------------
    added hit animation and movement
    implemented all the assets

--- austrian-political-parties/js/script.js
+++ austrian-political-parties/js/script.js
@@ -135,7 +135,7 @@ for(let i = 0; i<= maxFrame; i++){
       if(player1.dead == false){
         if(keys[65] && !player1.jumping){
           if(player1.lastDir == "l"){
-            ctx.drawImage(player1Sprite[1], player1.x, player2.y);
+            ctx.drawImage(player1Sprite[1], player1.x, player1.y);
           }
           else{
             ctx.drawImage(player1Sprite[2], player1.x, player1.y);
@@ -159,8 +159,8 @@ for(let i = 0; i<= maxFrame; i++){
         }
         else if(keys[81]){
           if(player1.lastDir == "l"){
-            //ctx.drawImage(player1Sprite[3],(player1.x - player1.width / 2),player1.y);
-            ctx.drawImage(player1Sprite[3],player1.x,player1.y);
+            ctx.drawImage(player1Sprite[3],(player1.x - player1.width / 2),player1.y);
+            //ctx.drawImage(player1Sprite[3],player1.x,player1.y);
             if ((player1.x - player1.range) <= (player1.x + player2.width) &&
               (player1.x - player1.range) >= player2.x  - (player2.width / 2) &&
               player1.y >= player2.y &&
@@ -170,17 +170,18 @@ for(let i = 0; i<= maxFrame; i++){
                     player2.velX -= player2.health;
                     player2.lastDir = "l";
             }
-            else{
-              ctx.drawImage(player1Sprite[4],player1.x,player1.y);
-              if((player1.x + player1.width) + player1.range >= player2.x &&
-                (player1.x + player1.width) + player1.range <= (player2.x + (player2.width * 1.5)) &&
-                player1.y >= player2.y &&
-                player1.y <= player2.y + player2.height) {
-                  hurt(player2, player1, healthP2);
-                  displayDamage(player2.health, "p2-damage");
-                  player2.velX += player2.health;
-                  player2.lastDir = "r";
-              }
+          }
+          else{
+            ctx.drawImage(player1Sprite[4],(player1.x + player1.width / 2),player1.y);
+            //ctx.drawImage(player1Sprite[4],player1.x,player1.y);
+            if((player1.x + player1.width) + player1.range >= player2.x &&
+              (player1.x + player1.width) + player1.range <= (player2.x + (player2.width * 1.5)) &&
+              player1.y >= player2.y &&
+              player1.y <= player2.y + player2.height) {
+                hurt(player2, player1, healthP2);
+                displayDamage(player2.health, "p2-damage");
+                player2.velX += player2.health;
+                player2.lastDir = "r";
             }
           }
         }
@@ -233,9 +234,9 @@ for(let i = 0; i<= maxFrame; i++){
           }
         }
         else if(keys[79]){
-          if(player1.lastDir == "l"){
-            //ctx.drawImage(player2Sprite[3],(player2.x - player2.width / 2),player2.y);
-            ctx.drawImage(player2Sprite[3],player2.x,player2.y);
+          if(player2.lastDir == "l"){
+            ctx.drawImage(player2Sprite[3],(player2.x - player2.width / 2),player2.y);
+            //ctx.drawImage(player2Sprite[3],player2.x,player2.y);
             if ((player2.x - player2.range) <= (player1.x + player1.width) &&
               (player2.x - player2.range) >= player1.x  - (player1.width / 2) &&
               player2.y >= player1.y &&
@@ -245,17 +246,18 @@ for(let i = 0; i<= maxFrame; i++){
                     player1.velX -= player1.health;
                     player1.lastDir = "l";
             }
-            else{
-              ctx.drawImage(player2Sprite[4],player2.x,player2.y);
-              if((player2.x + player2.width) + player2.range >= player1.x &&
-                (player2.x + player2.width) + player2.range <= (player1.x + (player1.width * 1.5)) &&
-                player2.y >= player1.y &&
-                player2.y <= player1.y + player1.height) {
-                  hurt(player1, player2, healthP1);
-                  displayDamage(player1.health, "p1-damage");
-                  player1.velX += player1.health;
-                  player1.lastDir = "r";
-              }
+          }
+          else{
+            ctx.drawImage(player2Sprite[4],(player2.x + player2.width / 2),player2.y);
+            //ctx.drawImage(player2Sprite[4],player2.x,player2.y);
+            if((player2.x + player2.width) + player2.range >= player1.x &&
+              (player2.x + player2.width) + player2.range <= (player1.x + (player1.width * 1.5)) &&
+              player2.y >= player1.y &&
+              player2.y <= player1.y + player1.height) {
+                hurt(player1, player2, healthP1);
+                displayDamage(player1.health, "p1-damage");
+                player1.velX += player1.health;
+                player1.lastDir = "r";
             }
           }
         }
